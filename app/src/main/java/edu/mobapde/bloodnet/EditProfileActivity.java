8b8f94ba740adbe3a1fa8d9c753bc1b5a5b513f5
psionic-nerd <ong.dionne03@gmail.com>
2017-03-18 01:34:40 +0800

    Added DatePickerDialog for Birthdate

--- app/src/main/java/edu/mobapde/bloodnet/EditProfileActivity.java
+++ app/src/main/java/edu/mobapde/bloodnet/EditProfileActivity.java
@@ -1,23 +1,47 @@
 package edu.mobapde.bloodnet;
 
+import android.app.DatePickerDialog;
 import android.content.DialogInterface;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
-public class EditProfileActivity extends AppCompatActivity {
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import edu.mobapde.bloodnet.fragments.DatePickerFragment;
+
+public class EditProfileActivity extends AppCompatActivity
+        implements DatePickerDialog.OnDateSetListener {
 
     Toolbar tbEdit;
+    Button btnBirthdate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
         tbEdit = (Toolbar) findViewById(R.id.tb_edit);
         setSupportActionBar(tbEdit);
-
+        btnBirthdate = ((Button) findViewById(R.id.btn_content_bday));
+        btnBirthdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog(v);
+            }
+        });
 
     }
 
@@ -57,4 +81,20 @@ public class EditProfileActivity extends AppCompatActivity {
 
         }
     }
+
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+        //do some stuff for example write on log and update TextField on activity
+        Log.w("DatePicker","Date = " + year);
+        Calendar date = new GregorianCalendar(year, month, day);
+        SimpleDateFormat format = new SimpleDateFormat("dd MMMM yyyy");
+        btnBirthdate.setText(format.format(date.getTime()));
+        btnBirthdate.setTextColor(ContextCompat.getColor(getBaseContext(), R.color.textPrimaryColor));
+    }
 }
+
---
+++ app/src/main/java/edu/mobapde/bloodnet/fragments/DatePickerFragment.java
@@ -0,0 +1,35 @@
+package edu.mobapde.bloodnet.fragments;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import java.util.Calendar;
+import java.util.Date;
+
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.widget.DatePicker;
+
+import edu.mobapde.bloodnet.EditProfileActivity;
+
+/**
+ * Created by psion on 3/18/2017.
+ */
+
+public class DatePickerFragment extends DialogFragment{
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current date as the default date in the picker
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog dialog = new DatePickerDialog(getActivity(), (EditProfileActivity)getActivity(), year, month, day);
+        dialog.getDatePicker().setMaxDate((new Date()).getTime());
+
+        // Create a new instance of  and return it
+        return dialog;
+    }
+
+}
