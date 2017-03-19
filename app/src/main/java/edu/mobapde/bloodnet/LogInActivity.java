3bcfe9cfa77c0e353663ff314e4e8e5a98546414
dionne-ong <dionne_ong@dlsu.edu.ph>
2017-03-19 16:29:02 +0800

    Registration & Log In via Firebase

--- app/src/main/java/edu/mobapde/bloodnet/LogInActivity.java
+++ app/src/main/java/edu/mobapde/bloodnet/LogInActivity.java
@@ -2,13 +2,22 @@ package edu.mobapde.bloodnet;
 
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LogInActivity extends AppCompatActivity {
 
@@ -16,35 +25,82 @@ public class LogInActivity extends AppCompatActivity {
     Button btnLogIn, btnRegister;
     ImageButton btnLogo;
 
+    private FirebaseAuth auth;
+    private ProgressBar progressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Get Firebase auth instance
+        auth = FirebaseAuth.getInstance();
+
+        if (auth.getCurrentUser() != null) {
+            startActivity(new Intent(LogInActivity.this, NavigationDrawerActivity.class));
+            finish();
+        }
+
         setContentView(R.layout.activity_log_in);
+
 
         TextView tv=(TextView)findViewById(R.id.tv_app_name);
         Typeface face= Typeface.createFromAsset(getAssets(),"fonts/Raleway Thin.ttf");
         tv.setTypeface(face);
-        
+
         btnLogIn = (Button) findViewById(R.id.btn_login);
         btnRegister = (Button) findViewById(R.id.btn_reg);
+        etEmail = (EditText) findViewById(R.id.et_email);
+        etPassword = (EditText) findViewById(R.id.et_password);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
 
+        //Get Firebase auth instance
+        auth = FirebaseAuth.getInstance();
 
         btnLogIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent();
-                i.setClass(getBaseContext(), NavigationDrawerActivity.class);
-                startActivity(i);
+
+                String email = etEmail.getText().toString();
+                final String password = etPassword.getText().toString();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                //authenticate user
+                auth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(LogInActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                // If sign in fails, display a message to the user. If sign in succeeds
+                                // the auth state listener will be notified and logic to handle the
+                                // signed in user can be handled in the listener.
+                                progressBar.setVisibility(View.GONE);
+                                if (!task.isSuccessful()) {
+                                        Toast.makeText(LogInActivity.this, task.getException().getLocalizedMessage(), Toast.LENGTH_LONG).show();
+
+                                } else {
+                                    Intent intent = new Intent(LogInActivity.this, NavigationDrawerActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                }
+                            }
+                        });
             }
         });
 
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent();
-                i.setClass(getBaseContext(), RegistrationActivity.class);
-
-                startActivity(i);
+                startActivity(new Intent(LogInActivity.this, RegistrationActivity.class));
             }
         });
 
