e40bfaa17e18bfe66ab7ad831bb22ba0e48463f2
Racheeeldj <christine_dejesus@dlsu.edu.ph>
2017-03-28 19:27:39 +0800

    carousel

--- app/src/main/java/edu/mobapde/bloodnet/FilterPostActivity.java
+++ app/src/main/java/edu/mobapde/bloodnet/FilterPostActivity.java
@@ -2,12 +2,33 @@ package edu.mobapde.bloodnet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
 
 public class FilterPostActivity extends AppCompatActivity {
+
+    CarouselView carouselView;
+
+    int[] sampleImages = {R.drawable.image1, R.drawable.image2, R.drawable.image3, R.drawable.image4, R.drawable.image5};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter_post);
+
+        carouselView = (CarouselView) findViewById(R.id.carouselView);
+        carouselView.setPageCount(sampleImages.length);
+
+        carouselView.setImageListener(imageListener);
     }
+
+    ImageListener imageListener = new ImageListener() {
+        @Override
+        public void setImageForPosition(int position, ImageView imageView) {
+            imageView.setImageResource(sampleImages[position]);
+        }
+    };
+
 }
