6f41f5edf2569d0eaa2d674ec98cb32594bc8cc6
Racheeeldj <christine_dejesus@dlsu.edu.ph>
2017-03-31 11:48:06 +0800

    Post

--- app/src/main/java/edu/mobapde/bloodnet/DBObjects/DBOPost.java
+++ app/src/main/java/edu/mobapde/bloodnet/DBObjects/DBOPost.java
@@ -8,4 +8,20 @@ public class DBOPost {
     public static final String POST_REF = "posts";
 
     public static final String EXTRA_POST_ID = "uuid";
+
+    public static final String O_POSITIVE = "o+";
+
+    public static final String O_NEGATIVE = "o-";
+
+    public static final String A_POSITIVE = "a+";
+
+    public static final String A_NEGATIVE = "a-";
+
+    public static final String B_POSITIVE = "b+";
+
+    public static final String B_NEGATIVE = "b-";
+
+    public static final String AB_POSITIVE = "ab+";
+
+    public static final String AB_NEGATIVE = "ab-";
 }
