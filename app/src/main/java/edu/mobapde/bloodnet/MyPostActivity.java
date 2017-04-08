ed4a139a08e7aa2831cb055a72cc444abcf4010f
katherine-07 <luisa_gilig@dlsu.edu.ph>
2017-04-08 10:50:47 +0800

    Images fixes

--- app/src/main/java/edu/mobapde/bloodnet/MyPostActivity.java
+++ app/src/main/java/edu/mobapde/bloodnet/MyPostActivity.java
@@ -94,6 +94,8 @@ public class MyPostActivity extends AppCompatActivity {
                         Glide.with(getBaseContext())
                                 .using(new FirebaseImageLoader())
                                 .load(pictureRef)
+                                .placeholder(getDrawable(R.drawable.imageerror1))
+                                .error(getDrawable(R.drawable.imageerror2))
                                 .into(imgBarPicture);
                     }
 
